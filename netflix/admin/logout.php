bbd47ba607d590c16fecf5dbc67362c2f10d247b
abeaar <140606965+abeaar@users.noreply.github.com>
2023-11-29 05:48:26 +0700

    Delete netflix directory

--- netflix/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // mengaktifkan session
-session_destroy(); // menghapus semua session
-// mengalihkan halaman sambil mengirim pesan logout
-header("location:../index.php?pesan=logout");
-?>
--- netflix/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = "localhost"; //hostname
-$username = "root"; //username untuk login ke mysql
-$password = ""; //password untuk login ke mysql
-$database = "netflix"; //nama database
-$konek = new mysqli($hostname, $username, $password, $database);
-
-?>
